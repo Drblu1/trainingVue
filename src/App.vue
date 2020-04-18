3cf120046c8cb7e5ade5d69c58929edb78faf61b
Yann <yannyavo@gmail.com>
2020-04-18 16:41:43 +0200

    First component training commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from "./Header.vue";
+    import ServersStatus from "./ServersStatus.vue";
+    import Footer from "./Footer.vue";
+    import ServerDetails from "./ServerDetails.vue";
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-servers': ServersStatus,
+            'app-server-details': ServerDetails,
+            'app-footer' : Footer,
+        }
+    }
+</script>
+
+<style>
+
+</style>
